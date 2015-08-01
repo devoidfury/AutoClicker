eb8a2a8a0c33161a28beacbb3d5e1ee6786f36d6
Tom Hunkapiller <tom@furycodes.com>
2015-08-01 03:41:20 -0500

    speed up a tiny bit by only printing in debug mode; reorganize a little

--- autoclicker.py
+++ autoclicker.py
@@ -5,15 +5,6 @@ from threading import *
 import time
 EVT_RESULT_ID = wx.NewId()
 
-def mouseClick(timer):
-    print "Click!"
-    x,y = win32api.GetCursorPos()
-    win32api.SetCursorPos((x, y))
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0) 
-    time.sleep(timer) 
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0) 
-    time.sleep(timer) 
-
 def EVT_RESULT(win, func):
     """Define Result Event."""
     win.Connect(-1, -1, EVT_RESULT_ID, func)
@@ -28,8 +19,9 @@ class ResultEvent(wx.PyEvent):
         
 class WorkerThread(Thread):
     '''Worker Thread Class.'''
-    def __init__(self, notify_window, timer):
+    def __init__(self, notify_window, timer, debug):
         Thread.__init__(self)
+        self.debug = debug
         self._notify_window = notify_window
         self._want_abort = False
         self.timer = timer
@@ -40,8 +32,17 @@ class WorkerThread(Thread):
             if self._want_abort:
                 wx.PostEvent(self._notify_window, ResultEvent(None))
                 return
-            mouseClick(self.timer)
-            print self.timer
+            self.mouseClick()
+            if self.debug:
+                print self.timer
+
+    def mouseClick(self):
+        x,y = win32api.GetCursorPos()
+        win32api.SetCursorPos((x, y))
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN,x,y,0,0) 
+        time.sleep(self.timer) 
+        win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0) 
+        time.sleep(self.timer) 
         
     def abort(self):
         self._want_abort = True
@@ -52,15 +53,19 @@ class Frame1(wx.Frame):
         wx.Frame.__init__(self, *args, **kwargs)
         
         self.autoClick = False
+        self.debug = False
         self.worker = None
         self.__set_properties()
         self.regHotKey()
         self.Bind(wx.EVT_HOTKEY, self.handleHotKey, id=self.hotKeyId)
         self.slider1 = wx.Slider(self, -1, 1, 1, 10000, (10, 10), (300, 50),
             wx.SL_HORIZONTAL | wx.SL_AUTOTICKS | wx.SL_LABELS)
+        self.debug_checkbox = wx.CheckBox(self, -1, "Debug Mode", (10, 60))
+
+
     def __set_properties(self):
         self.SetTitle("AutoClicker")
-        self.SetSize((300, 100))
+        self.SetSize((300, 200))
         self.SetBackgroundColour("white")
         
     def regHotKey(self):
@@ -72,14 +77,21 @@ class Frame1(wx.Frame):
             self.hotKeyId, #a unique ID for this hotkey
             win32con.MOD_ALT, #the modifier key
             win32con.VK_F1) #the key to watch for
+
     def handleHotKey(self, evt):
+        self.debug = self.debug_checkbox.GetValue();
         self.autoClick = not self.autoClick
         if self.autoClick:
-            self.worker = WorkerThread(self, float(1/float(self.slider1.GetValue())))
+            self.worker = WorkerThread(
+                notify_window=self, 
+                timer=float(1/float(self.slider1.GetValue())),
+                debug=self.debug
+            )
         else:
             self.worker.abort()
             self.worker = None
-        print self.autoClick
+
+        print 'AutoClicker ON' if self.autoClick else 'AutoClicker OFF'
 
 class AutoClicker(wx.App):
     def OnInit(self):
